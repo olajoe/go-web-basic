74f62a103103c0d40d25c2179c4f11a585672783
Jaturon <giorojoe@gmail.com>
2018-05-31 16:28:03 +0700

    templates

--- app.go
+++ app.go
@@ -1,23 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type clubDetail struct {
+	ClubName       string
+	Address        string
+	LeagueChampion bool
+}
+
+type clubData struct {
+	CountryName string
+	ClubDetails []clubDetail
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	country := vars["name"]
-	club := vars["clubName"]
-	fmt.Fprintf(w, "You favorite club is %s in %s", club, country)
+	tmp := template.Must(template.ParseFiles("display.html"))
+	data := clubData{
+		CountryName: "Italy",
+		ClubDetails: []clubDetail{
+			{ClubName: "AC Milan", Address: "Milan, Italy", LeagueChampion: true},
+			{ClubName: "Atalanta", Address: "Bergamo Italy", LeagueChampion: false},
+			{ClubName: "Juventus", Address: "Turin, Italy", LeagueChampion: true},
+		},
+	}
+	tmp.Execute(w, data)
+
 }
 
 func main() {
-	r := mux.NewRouter()
 
-	r.HandleFunc("/countries/{name}/clubs/{clubName}", indexHandler).Methods("GET")
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler).Methods("GET")
 
 	http.ListenAndServe(":8000", r)
 }
