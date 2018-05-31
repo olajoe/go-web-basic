47058d166778437d9be52e265a38c6a71f02d62c
Jaturon <giorojoe@gmail.com>
2018-06-01 01:16:33 +0700

    refactor code

--- app.go
+++ app.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"html/template"
+	"joe/joe-go-api/handlers"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
-
-type clubDetail struct {
-	ClubName       string
-	Address        string
-	LeagueChampion bool
-}
-
-type clubData struct {
-	CountryName string
-	ClubDetails []clubDetail
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tmp := template.Must(template.ParseFiles("display.html"))
-	data := clubData{
-		CountryName: "Italy",
-		ClubDetails: []clubDetail{
-			{ClubName: "AC Milan", Address: "Milan, Italy", LeagueChampion: true},
-			{ClubName: "Atalanta", Address: "Bergamo Italy", LeagueChampion: false},
-			{ClubName: "Juventus", Address: "Turin, Italy", LeagueChampion: true},
-		},
-	}
-	tmp.Execute(w, data)
-
-}
 
 func main() {
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
+	router := handlers.Router()
 
-	http.ListenAndServe(":8000", r)
+	http.ListenAndServe(":8000", router)
 }
