90fa866aa8e87540aa2d86eb5d36f9017a5d688d
Jaturon <giorojoe@gmail.com>
2018-05-31 14:29:27 +0700

    HTTP Server

--- app.go
+++ app.go
@@ -6,10 +6,14 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hey Welcome to Go! %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Hey Welcome to my website your path is: %s\n", r.URL.Path)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.ListenAndServe(":8000", nil)
 }
