2523002f42e648a687b89032af8435dd92611606
Jaturon <giorojoe@gmail.com>
2018-06-01 16:04:16 +0700

    add logging middleware

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,23 @@
 package handlers
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method + ": " + r.URL.Path)
+		f(w, r)
+	}
+
+}
+
 func Router() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/home", homePage).Methods("GET")
-	r.HandleFunc("/form", formContact).Methods("POST")
+	r.HandleFunc("/home", logging(homePage)).Methods("GET")
+	r.HandleFunc("/form", logging(formContact)).Methods("POST")
 	return r
 }
